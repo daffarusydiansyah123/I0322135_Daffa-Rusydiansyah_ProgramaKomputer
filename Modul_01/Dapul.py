7fa1ce0d187776695d71b2fd3cf6a5270eefd1fe
Daffa <daffarusydiansyah@student.uns.ac.id>
2023-03-06 10:05:00 +0700

    commit pertama

---
+++ Modul_01/Dapul.py
@@ -0,0 +1,13 @@
+nama = "Daffa"
+lengkap = "Rusydiansyah"
+
+angka = 10
+desimal = 0.5
+boolean = True
+Kelas_D = ["nama": "Daffa", "Kelas": "Kelas D"]
+dictionary = {"nama": "daffa", "Kelas": "KLS D", "NIM":135}
+
+print(nama_depan)
+print("nama saya adalah Daffa")
+
+print(f"nama saya adalah {nama} {Lengkap} dari kelas {dictionary[ 'kelas' ]}")
