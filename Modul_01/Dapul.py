c8df48a19cb9caaad0ad905864b359a1ca6db811
Daffa <daffarusydiansyah@student.uns.ac.id>
2023-03-07 19:32:05 +0700

    tugas

--- Modul_01/Dapul.py
+++ Modul_01/Dapul.py
@@ -5,9 +5,9 @@ angka = 10
 desimal = 0.5
 boolean = True
 Kelas_D = ["nama" : "Daffa", "Kelas" : " D"]
-dictionary = {"nama": "daffa", "Kelas": "KLS D", }
+data = {"nama": "daffa", "Kelas": "KLS D", }
 
 print("nama_depan")
 print("nama saya adalah Daffa")
 
-print("nama saya adalah (nama depan) (nama belakang) dari kelas (dictionary[ 'kelas' ])")
+print("nama saya adalah (nama depan) (nama belakang) dari kelas (data [ "kelas" ])")
