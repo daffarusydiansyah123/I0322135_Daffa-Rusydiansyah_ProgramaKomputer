ccdf117b7911f1b4577c77ebeade9ac11175c704
Daffa <daffarusydiansyah@student.uns.ac.id>
2023-03-11 22:44:00 +0700

    Exercise

---
+++ Modul_01/Daffa.py
@@ -0,0 +1,13 @@
+nama_depan = "Daffa"
+nama_belakang = "Rusydiansyah"
+
+angka = 10
+desimal = 0.5
+boolean = True
+Kelas_D = ["nama" : "Daffa", "Kelas" : " D"]
+data = {"nama": "daffa", "Kelas": "KLS D", }
+
+print("nama_depan")
+print("nama saya adalah Daffa")
+
+print("nama saya adalah (nama depan) (nama belakang) dari kelas (data [ "kelas" ])")
